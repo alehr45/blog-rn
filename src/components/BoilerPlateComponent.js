144410419dd8f4673c6d58205f6132cc446a63e8
Alex Lehr <alexsmacbook@Alexs-MacBook-Air.local>
2022-07-04 07:15:30 -0500

    first commit

---
+++ src/components/BoilerPlateComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native"
+
+
+const BoilerPlateComponent = () => {
+    return (
+        <View>
+            <Text>BoilerPlateComponent</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default BoilerPlateComponent
